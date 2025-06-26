00a2a3bc0113e6cc2214ff7b0bc7685ea360841c
Qin Yu <qin.yu.95@outlook.com>
2025-06-26 17:45:47 +0000

    chore: bump version 0.0.4 → 0.0.5

--- stardist/setup.py
+++ stardist/setup.py
@@ -1,20 +1,30 @@
-from setuptools import setup, find_packages
+from pathlib import Path
 
-exec(open('runstardist/__version__.py', encoding='utf-8').read())  # pylint: disable=exec-used
+from setuptools import find_packages, setup
+
+
+exec(open("runstardist/__version__.py", encoding="utf-8").read())
 
 setup(
-    name='run-stardist',
-    version=__version__,  # pylint: disable=undefined-variable
-    author='Qin Yu',
-    author_email='qin.yu@embl.de',
-    license='MIT',
-    description='Train and use StarDist models',
-    url='https://github.com/kreshuklab/go-nuclear',
+    name="run-stardist",
+    version=__version__,
+    author="Qin Yu",
+    author_email="qin.yu@embl.de",
+    license="MIT",
+    description="Train and use StarDist models",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kreshuklab/go-nuclear",
+    project_urls={
+        "Documentation": "https://kreshuklab.github.io/go-nuclear/",
+        "Source": "https://github.com/kreshuklab/go-nuclear",
+        "Bug Tracker": "https://github.com/kreshuklab/go-nuclear/issues",
+    },
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'train-stardist=runstardist.train:main',
-            'predict-stardist=runstardist.predict:main',
+        "console_scripts": [
+            "train-stardist=runstardist.train:main",
+            "predict-stardist=runstardist.predict:main",
         ],
     },
 )
